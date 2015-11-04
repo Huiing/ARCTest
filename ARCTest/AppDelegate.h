23d8b678fdd4a455febb54ae8031c33c6c76145a
吴东辉 <wudh@n2n2n.com>
2015-11-05 00:13:44 +0800

    Initial Commit

---
+++ ARCTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARCTest
+//
+//  Created by faith on 15/11/5.
+//  Copyright (c) 2015年 faith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
